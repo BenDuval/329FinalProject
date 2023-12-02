09513d5ee38ad761ae87bcdd04808593dc3d7f46
BenDuval <152676776+BenDuval@users.noreply.github.com>
2023-12-02 07:47:58 -0800

    Create EEPROM.h

---
+++ EEPROM.h
@@ -0,0 +1,25 @@
+/*
+ * EEPROM.h
+ *
+ *  Created on: Nov 30, 2023
+ *      Author: bcstu
+ */
+
+#ifndef INC_EEPROM_H_
+#define INC_EEPROM_H_
+
+#define EEPROM_ADDRESS 0x50
+#define I2C1_AF_NUMBER 4
+#define TEST_ADDRESS1 0x1ABC
+#define TEST_ADDRESS2 0x2BCD
+#define TEST_ADDRESS3 0x3CDE
+#define TEST_ADDRESS4 0x4DEF
+
+#define TEST_DATA1 0x5A
+#define TEST_DATA2 0xA5
+#define TEST_DATA3 0x55
+#define TEST_DATA4 0xAA
+#define TIMEOUT 10000
+#define EEPROM_MEMORY_ADDR 0x01
+# define byte 7f
+#endif /* INC_EEPROM_H_ */
